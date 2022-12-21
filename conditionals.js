bcc38cc6d42689a7a5dd3abc1cd25fb188ff3a20
Wo Yong Jeong <woyong.jeong@gmail.com>
2022-12-21 11:18:24 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -10,3 +10,26 @@ if (cold) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
